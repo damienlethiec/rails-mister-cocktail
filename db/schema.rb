ba2e5ef1ac2afe00c5af799badd1daaf7bb584a1
Damien Le Thiec <damien.lethiec@gmail.com>
2016-08-11 16:23:21 +0200

    début front

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811103015) do
+ActiveRecord::Schema.define(version: 20160811141331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160811103015) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "picture"
   end
 
   add_foreign_key "doses", "cocktails"
