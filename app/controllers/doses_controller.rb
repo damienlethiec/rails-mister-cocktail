ea999dee12e0b0d70bf572a184a8c16d46e5dffb
Damien Le Thiec <damien.lethiec@gmail.com>
2016-08-11 14:56:00 +0200

    quasi fin back end

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,31 @@
 class DosesController < ApplicationController
+  before_action :find_cocktail, only: [ :new, :create ]
 
   def new
-
+    @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.delete
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
